a0ecd36d0c1bb6a99233907280b57c7000ff3436
Chetan Thosare <chetan.speck@gmail.com>
2024-03-16 01:11:16 +0530

    feat: Implemented basic frontend setup in React with TypeScript

---
+++ README.md
@@ -0,0 +1,3 @@
+# Notes
+
+1. Create ".env.production" file for production version with similar environment variables as in ".env.development".
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+// -------------------------------------------------------------------------------------------
+
+import './styles/animation.scss';
+import './styles/color.scss';
+import './styles/elevation.scss';
+import './styles/global.scss';
+import './styles/layout.scss';
+import './styles/motion.scss';
+import './styles/shape.scss';
+import './styles/scroll.scss';
+import './styles/typography.scss';
+
+// -------------------------------------------------------------------------------------------
+
+const App = () => {
+	return <></>;
+};
+
+// -------------------------------------------------------------------------------------------
+
+export default App;
+
+// -------------------------------------------------------------------------------------------
